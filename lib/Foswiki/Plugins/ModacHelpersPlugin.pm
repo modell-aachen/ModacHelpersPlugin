551000d5ec2fa2b9b12e0460be6c9633ea3402e4
mmoscher <moscher@modell-aachen.de>
2018-06-28 16:02:24 +0200

    id: 5429 added REST Handler to fetch Webs and WebTopics

--- lib/Foswiki/Plugins/ModacHelpersPlugin.pm
+++ lib/Foswiki/Plugins/ModacHelpersPlugin.pm
@@ -6,11 +6,27 @@ use warnings;
 
 use Foswiki::Plugins;
 use Foswiki::UI::Rename;
+use JSON;
+use Foswiki::AccessControlException;
 
 our $VERSION = "1.0";
 our $RELEASE = "1.0";
 our $SHORTDESCRIPTION = 'This plugin provides several helper functions.';
 our $NO_PREFS_IN_TOPIC = 1;
+
+sub initPlugin {
+  Foswiki::Func::registerRESTHandler('webs', \&_handleRESTWebs,
+    authenticate  => 1,
+    http_allow    => 'GET',
+    description   => "Handler to fetch all Web's for current user"
+  );
+
+  Foswiki::Func::registerRESTHandler('topics', \&_handleRESTWebTopics,
+    authenticate  => 1,
+    http_allow    => 'GET',
+    description   => "Handler to fetch all Topics for a given web name"
+  );
+}
 
 sub updateTopicLinks {
   my ($oldWeb, $oldTopic, $newWeb, $newTopic) = @_;
@@ -63,6 +79,50 @@ sub rewriteLinks {
     $renderer->forEachLine( $topicObject->text(), \&Foswiki::UI::Rename::_replaceTopicReferences, $rewriteLinkOptions );
   $topicObject->text($text);
   $topicObject->save( minor => 1 );
+}
+
+sub _handleRESTWebs {
+
+  # $filter
+  #  - 'user' == only user webs (hide hidden once, e.g. _empty)
+  #  - 'public' == filter all public webs
+  #  - 'allowed' == exclude all webs the current user can't read
+  my @webs = Foswiki::Func::getListOfWebs( "user,public,allowed" );
+  return to_json(\@webs);
+}
+
+sub _handleRESTWebTopics {
+  my ( %session, undef, undef, $response ) = @_;
+  my $request = Foswiki::Func::getRequestObject();
+  my $web = $request->param("webname");
+
+  die unless $web;
+
+  my @webTopics = ();
+  if( Foswiki::Func::webExists( $web ) ) {
+    @webTopics = Foswiki::Func::getTopicList( $web );
+  }
+  # filter topic names and build full-qualified object
+  # webTopic = { title: 'Sample Web Name', name: 'SampleWebName', web: 'Processes' }
+  my @filteredWebTopics = ();
+  foreach(@webTopics) {
+    my %webTopic;
+    $webTopic{name} = $_;
+    $webTopic{web} = $web;
+
+    if( Foswiki::Func::checkAccessPermission( "VIEW", $session{user}, undef, $webTopic{name}, $webTopic{web} ) ) {
+      my ($topicMeta, $text) = Foswiki::Func::readTopic($webTopic{web}, $webTopic{name});
+      if( $topicMeta->get( 'FIELD', 'TopicTitle' ) ) {
+        $webTopic{title} = $topicMeta->get( 'FIELD', 'TopicTitle' )->{value};
+      }else{
+        $webTopic{title} = $webTopic{name};
+      }
+    }
+
+    push @filteredWebTopics, {%webTopic};
+  }
+
+  return to_json(\@filteredWebTopics);
 }
 
 1;
